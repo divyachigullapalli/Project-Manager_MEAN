835e62f40db7b05ac327263b7f05de37f940c863
CTS\602814 <divya.x.chigullapalli@questdiagnostics.com>
2019-05-08 11:33:12 +0530

    added app comp spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,46 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { FilterPipe} from './pipes/filter.pipe';
+import { OrderModule } from 'ngx-order-pipe'; 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ViewTaskComponent } from './components/view-task/view-task.component';
+import { BackendApiService } from './services/backend-api.service';
+import { RouterModule, Routes } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        AddUserComponent,
+        AddProjectComponent,
+        AddTaskComponent,
+        ViewTaskComponent,
+        FilterPipe
       ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        OrderModule,
+        RouterModule.forRoot(
+          [ { path: '', component: AddUserComponent },
+          { path: 'adduser', component: AddUserComponent }, 
+          { path: 'addproject', component: AddProjectComponent },
+          { path: 'addtask', component: AddTaskComponent },
+          { path: 'viewtask', component: ViewTaskComponent }
+        ]
+        )
+      ],
+      providers: [BackendApiService]
     }).compileComponents();
   }));
 
